55668d5b98b5440a5c01b2eb4c77762b2edd8154
Nnanyielugo <kz.onyegbule@gmail.com>
2018-07-05 15:49:04 +0100

    fixed variable error in auth reducer

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -2,7 +2,7 @@ import { AUTH_SET_TOKEN, AUTH_REMOVE_TOKEN } from '../actions/actionTypes';
 
 const initialState = {
   token: null,
-  expiryDate
+  expiryDate: null
 }
 const reducer = (state = initialState, action) => {
   switch (action.type) {
